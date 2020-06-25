df7f90a1f2b7f3f1601ac861091eb1b3b761d3ca
Paulo <paulo.leite@academico.ifpb.edu.br>
2020-06-24 23:36:45 -0300

    Finalizado

--- src/database/db.js
+++ src/database/db.js
@@ -7,27 +7,75 @@ sqlite3.verbose()
 const db = new sqlite3.Database('./src/database/database.db')
 
 // utilizar o objeto de banco de dados, para nossas operações
-db.serialize(() => {
-    //Criar uma tabela
+// db.serialize(() => {
+//     //Criar uma tabela
 
-    db.run(`
-        CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT,
-            address TEXT,
-            address2 TEXT,
-            state TEXT,
-            city TEXT,
-            items TEXT
-        );        
-    `)
-
-
-    //Inserir dados na tabela
+//     db.run(`
+//         CREATE TABLE IF NOT EXISTS places (
+//             id INTEGER PRIMARY KEY AUTOINCREMENT,
+//             image TEXT,
+//             name TEXT,
+//             address TEXT,
+//             address2 TEXT,
+//             state TEXT,
+//             city TEXT,
+//             items TEXT
+//         );        
+//     `)
 
 
-    //Consultar os dados da tabela
+//     //Inserir dados na tabela
+//     const query = `
+//     INSERT INTO places (
+//         image,
+//         name,
+//         address,
+//         address2,
+//         state,
+//         city,
+//         items
+//         ) VALUES (?,?,?,?,?,?,?);`
 
 
-    //Deletar um dado da tabela
-})
+//     const values = [
+//         "https://images.unsplash.com/photo-1481761289552-381112059e05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=861&q=80",
+//         "Paperside",
+//         "Guilherme Gemballa, Jardim América",
+//         "Número 260",
+//         "Santa Catarina",
+//         "Rio do Sul",
+//         "Resíduos Eletrônicos, Lâmpadas"
+//     ]
+
+//     function afterInsertData(err) {
+//         if (err)
+//             return console.log(err)
+
+//         console.log("Cadastrado com sucesso")
+//         console.log(this)
+//     }
+
+//     //Executa a callback quando terminar o que foi pedido
+//     db.run(query, values, afterInsertData)
+
+//Consultar os dados da tabela
+// db.all(`SELECT * FROM places`, function (err, rows) {
+//     if (err)
+//         return console.log(err)
+
+//     console.log('Aqui estão seus registros: ')
+//     console.log(rows)
+// })
+
+//Deletar um dado da tabela
+// db.run(`DELETE FROM places WHERE id = ?`, [3], function (err) {
+//     if (err)
+//         return console.log(err)
+
+//     console.log('Registro deletado com sucesso')
+// })
+
+
+// })
+
+module.exports = db
--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+// pegar o banco de dados
+const db = require('./database/db.js')
+
+// habilitar o uso do req.body na nossa aplicação
+app.use(express.urlencoded({
+    extended: true
+}))
 
 //configurar rotas(caminhos)
 //página inicial
@@ -12,12 +19,92 @@ app.get('/', (req, res) => {
 })
 
 app.get('/create-point', (req, res) => {
-    console.log(__dirname)
+
+    //req.query: Query strings da url
+    //console.log(req.query)
+
     return res.render('create-point.html')
 })
 
+app.post('/savepoint', (req, res) => {
+
+    //req.body: o corpo do nosso formulário
+    //console.log(req.body)
+
+    //Inserir dados na tabela
+    const query = `
+    INSERT INTO places (
+        image, 
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+        ) VALUES (?,?,?,?,?,?,?);`
+
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            res.send('Erro no cadastro')
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render('create-point.html', {
+            saved: true
+        })
+    }
+
+    //Executa a callback quando terminar o que foi pedido
+    db.run(query, values, afterInsertData)
+
+})
+
 app.get('/search', (req, res) => {
-    return res.render('search-results.html')
+
+    const search = req.query.search
+
+    if (search == '') {
+        //pesquisa vazia
+        // mostrar a página html com os dados do banco de dados
+        return res.render('search-results.html', {
+            total: 0
+        })
+    }
+
+    // pegar os dados do banco de dados
+    // '%${search}%' -> qualquer coisa antes ou depois do valor de 'search'
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        if (err)
+            return console.log(err)
+
+
+        const total = rows.length
+
+        console.log('Aqui estão os registros: ')
+        console.log(rows)
+
+        // mostrar a página html com os dados do banco de dados
+        return res.render('search-results.html', {
+            places: rows,
+            total: total
+        })
+
+    })
+
+
 })
 
 //Configurar pasta pública
